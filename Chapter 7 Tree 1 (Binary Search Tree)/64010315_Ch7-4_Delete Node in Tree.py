778dff3ccb7ce80a1fdfd5f05ffc61098de2b04c
64010315 Thanathon Tangon <88655183+FiMart@users.noreply.github.com>
2023-04-21 12:50:45 +0700

    Add files via upload

--- Chapter 7 Tree 1 (Binary Search Tree)/64010315_Ch7-4_Delete Node in Tree.py
+++ Chapter 7 Tree 1 (Binary Search Tree)/64010315_Ch7-4_Delete Node in Tree.py
@@ -57,33 +57,68 @@ class BinarySearchTree:
         self.root = None
 
     def insert(self, val):
+        a = Node(val)
         if self.root is None:
-            self.root = Node(val)
+            self.root = a
             return self.root
-        node = self.root
-        while True:
-            if val < node.data:
-                if node.left == None:
-                    node.left = Node(val)
-                    return self.root
-                node = node.left
-            else:
-                if node.right == None:
-                    node.right = Node(val)
-                    return self.root
-                node = node.right
+        else:
+            t=self.root
+            while True:
+                if val <= t.data:
+                    if t.left is None:
+                        t.left = a
+                        return self.root
+                    else:
+                        t = t.left
+                else:
+                    if t.right is None:
+                        t.right = a
+                        return self.root
+                    else:
+                        t = t.right
 
     def delete(self, r, data):
-        # code here
+        if r is None:
+            print("Error! Not Found DATA")
+            return
+        if r.data != data: 
+            if data<r.data:
+                r.left = self.delete(r.left, data) 
+            elif data> r.data:
+                r.right = self.delete(r.right, data)  
+        else:   
 
+            if r.left is None:   
+                r = r.right
+                return r
+            elif r.right is None:  
+                r = r.left
+                return r
+            else:
+                current = r.right
+                while current.left is not None:
+                    current = current.left
 
-def printTree90(node, level=0):
-    if node != None:
-        printTree90(node.right, level + 1)
-        print('     ' * level, node)
-        printTree90(node.left, level + 1)
+                r.data = current.data    
+                
+                r.right = self.delete(r.right, current.data)
+        return r
+
+    def printTree90(self,node, level=0):
+        if node != None:
+            self.printTree90(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree90(node.left, level + 1)
 
 
 tree = BinarySearchTree()
 val = input("Enter Input : ").split(",")
-# code here
+for i in range(len(val)):
+    if val[i][0] == 'i':
+        print("insert " + (val[i][2:]))
+        tree.root = tree.insert(int(val[i][2:]))
+        tree.printTree90(tree.root)
+    if val[i][0] == 'd':
+        print("delete " + (val[i][2:]))
+        tree.root = tree.delete(tree.root, int(val[i][2:]))
+        tree.printTree90(tree.root)
